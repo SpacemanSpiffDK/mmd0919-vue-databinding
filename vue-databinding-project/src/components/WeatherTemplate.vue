bab6f18f94b7f4d3041b4ff5d44f3b74a9bf44bf
Dan Høegh <dan.hoegh@gmail.com>
2020-10-16 14:33:11 +0200

    Update WeatherTemplate.vue

--- vue-databinding-project/src/components/WeatherTemplate.vue
+++ vue-databinding-project/src/components/WeatherTemplate.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-text="info"></div>
+    <div v-text="data"></div>
   </div>
 </template>
 
@@ -9,8 +9,35 @@ export default {
   name: 'WeatherTemplate',
   data() {
     return {
-      info: 'test'
     };
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      
+      let data = {};
+      function getData(){
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            try {
+              data = JSON.parse(this.responseText);
+              // outputResult(data);
+            } catch (error){
+              errorMessage(`Error parsing JSON: <span class="errorMsg">${error}</span>`);
+            }
+          }
+          if (this.readyState == 4 && this.status > 400) {
+            errorMessage("An error occured getting the data, please try again later"); 
+          }
+        };
+        xhttp.open("GET", `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=braben`, true);
+        xhttp.send();
+      }
+      function errorMessage(str) {
+        console.log('Error:' + str);
+      }
+      getData();
+    });
   },
 }
 </script>
